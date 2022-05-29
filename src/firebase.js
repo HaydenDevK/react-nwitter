fbf3e3b80e0c4359b4237097aefad42b61db2ee6
sophie-human-k <sophie.human.k@gmail.com>
2022-05-29 15:32:14 +0900

    1.0 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAb0tmiFOko7y7Mjkm1CAz2hWtiUv_qVlI",
+  authDomain: "nwitter-49256.firebaseapp.com",
+  projectId: "nwitter-49256",
+  storageBucket: "nwitter-49256.appspot.com",
+  messagingSenderId: "211342271628",
+  appId: "1:211342271628:web:2b033226c70df69ec787a9"
+};
+
+export default firebase.initializeApp(firebaseConfig);
